a781aa26e6b2d8333141a5a5abf5a4b9446088c5
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-14 01:43:10 +0600

    google sign in

---
+++ src/components/Login/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
+
+const Login = () => {
+    const {signInWithGoogle} = useFirebase();
+    return (
+        <div>
+            <h2>please Login</h2>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form action="">
+                <input type="email" name="" id="" placeholder='Your Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Your Password' />
+                <br />
+                <input type="submit" value="Login" />
+
+            </form>
+        </div>
+    );
+};
+
+export default Login;
